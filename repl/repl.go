3f4e7bbe26b1a9759c5e7a32f8ebad51cda6d04d
Flily Hsu <1126143+flily@users.noreply.github.com>
2023-03-13 19:14:07 +0800

    Fix code style warning

--- repl/repl.go
+++ repl/repl.go
@@ -15,7 +15,7 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprintf(out, PROMPT)
+		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
