1549e098c9fd25d95b6f92c3e97c8497769094f2
Flily Hsu <1126143+flily@users.noreply.github.com>
2023-03-14 16:33:00 +0800

    finish chapter 2.5, add support for return statement

--- ast/ast.go
+++ ast/ast.go
@@ -46,3 +46,11 @@ type Identifier struct {
 
 func (ls *Identifier) expressionNode()      {}
 func (ls *Identifier) TokenLiteral() string { return ls.Token.Literal }
+
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
--- parser/parser.go
+++ parser/parser.go
@@ -82,6 +82,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -105,7 +107,23 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// TODO: We're skipping the expressions until comma
+	// TODO: We're skipping the expressions until semicolon.
+	// we'll come back to this later
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{
+		Token: p.curToken,
+	}
+
+	p.nextToken()
+
+	// TODO: We're skipping the expressions until semicolon.
 	// we'll come back to this later
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
