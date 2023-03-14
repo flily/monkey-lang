13096ddbf1d75d4a73f25d37b33c5f08ae1c0a1e
Flily Hsu <1126143+flily@users.noreply.github.com>
2023-03-14 16:05:15 +0800

    finish chapter 2.4, add error collection for parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,26 +1,42 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/flily/monkey-lang/ast"
 	"github.com/flily/monkey-lang/lexer"
 	"github.com/flily/monkey-lang/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -42,6 +58,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		return true
 
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
