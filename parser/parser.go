18865360634bf9dafef8e9d80e8d3d04d0ea5839
Flily Hsu <1126143+flily@users.noreply.github.com>
2023-03-15 22:30:42 +0800

    remove test code in nextToken()

--- parser/parser.go
+++ parser/parser.go
@@ -94,7 +94,6 @@ func (p *Parser) noPrefixParseFnError(t token.TokenType) {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
-	fmt.Printf("[ %s ] < %s >\n", p.curToken.Literal, p.peekToken.Literal)
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
