fecd6a3bdd49351e1d03fd19750a008be07ee5c7
Flily Hsu <1126143+flily@users.noreply.github.com>
2023-03-27 14:21:07 +0800

    ch8.3, add support for compiling builtin functions

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -3,8 +3,9 @@ package compiler
 type SymbolScope string
 
 const (
-	LocalScope  SymbolScope = "LOCAL"
-	GlobalScope SymbolScope = "GLOBAL"
+	LocalScope   SymbolScope = "LOCAL"
+	GlobalScope  SymbolScope = "GLOBAL"
+	BuiltinScope SymbolScope = "BUILTIN"
 )
 
 type Symbol struct {
@@ -47,6 +48,17 @@ func (s *SymbolTable) Define(name string) Symbol {
 
 	s.store[name] = symbol
 	s.numDefinitions++
+	return symbol
+}
+
+func (s *SymbolTable) DefineBuiltin(index int, name string) Symbol {
+	symbol := Symbol{
+		Name:  name,
+		Scope: BuiltinScope,
+		Index: index,
+	}
+
+	s.store[name] = symbol
 	return symbol
 }
 
